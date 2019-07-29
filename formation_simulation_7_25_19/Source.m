19d5e302b151f6711034c92e03dce695633e32fe
victor-qin <31549074+victor-qin@users.noreply.github.com>
2019-07-29 17:47:49 -0400

    Attempted fixes on Ogren algorithm

--- formation_simulation_7_25_19/Source.m
+++ formation_simulation_7_25_19/Source.m
@@ -10,9 +10,9 @@ classdef Source
        end
        function r = Reward(obj, pos)    % return reward from a certain position - 1/r^2
            var = 0.001;
-           temp = sum(pos - obj.state(1:end-1));
-           r = -obj.state(end)*4/((1+exp(0.5*temp))*(1+exp(-0.5*temp))) + var*randn();
-%            r = -obj.state(end)/sum(temp) + var*randn();
+           temp = norm(pos - obj.state(1:end-1));
+%            r = -obj.state(end)*4/((1+exp(0.5*temp))*(1+exp(-0.5*temp))) + var*randn();
+           r = -obj.state(end)/temp + var*randn();
        end
        function r = returnPos(obj)
           r = obj.state(1:2);
