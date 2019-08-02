15cf6775e845de4557632b5746409ade5a0f775d
victor-qin <31549074+victor-qin@users.noreply.github.com>
2019-08-02 12:11:27 -0400

    Formation stuff

--- formation_simulation_7_25_19/Source.m
+++ formation_simulation_7_25_19/Source.m
@@ -11,8 +11,8 @@ classdef Source
        function r = Reward(obj, pos)    % return reward from a certain position - 1/r^2
            var = 0.001;
            temp = norm(pos - obj.state(1:end-1));
-%            r = -obj.state(end)*4/((1+exp(0.5*temp))*(1+exp(-0.5*temp))) + var*randn();
-           r = -obj.state(end)/temp + var*randn();
+           r = -obj.state(end)*4/((1+exp(0.5*temp))*(1+exp(-0.5*temp))) + var*randn();
+%            r = -obj.state(end)/temp + var*randn();
        end
        function r = returnPos(obj)
           r = obj.state(1:2);
