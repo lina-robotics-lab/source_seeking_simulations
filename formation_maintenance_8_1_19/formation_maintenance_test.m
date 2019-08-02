8891c3d8e72c7190e88501235ab4f00351f31a0a
victor-qin <lingheng.qin@gmail.com>
2019-08-02 09:40:02 -0400

    Success with formation maintenance and movement

--- formation_maintenance_8_1_19/formation_maintenance_test.m
+++ formation_maintenance_8_1_19/formation_maintenance_test.m
@@ -2,14 +2,16 @@ syms f_d(d) f_h(d)
 t = 0.1;
 
 a_d = 1;
+d_1 = 4;
 d_0 = sqrt(2)*sqrt(3);
-f_d(d) = a_d*(1/d - d_0/d^2);
-d_def = f_d(4);
+f_d = @(d)a_d*(1/d - d_0/d^2);
+d_def = feval(f_d, d_1);
 
-a_h = 4;
+a_h = 1;
+h_1 = 4;
 h_0 = sqrt(2);
-f_h(d) = a_h*(1/d - h_0/d^2);
-h_def = f_h(4);
+f_h = @(d)a_h*(1/d - h_0/d^2);
+h_def = feval(f_h, h_1);
 
 source1 = Source(0, 0, 1);
 % Robots
@@ -31,14 +33,20 @@ robot3.addState([pos, reward]);
 robotList = [robot1 robot2 robot3];
 
 % Virtual leaders
-virtLead = RobotAgent;
+virtLead1 = RobotAgent;
 pos = [0,0];
 reward = source1.Reward(pos);
-virtLead.addState([pos, reward]);
+virtLead1.addState([pos, reward]);
 
-virtList = [virtLead];
+virtLead2 = RobotAgent;
+pos = [0,0.5];
+reward = source1.Reward(pos);
+virtLead2.addState([pos, reward]);
+
+virtList = [virtLead1];
 
 K_p = 0.1;
+vel_max = 0.1;
 % Iterative steps
 for iter = 1:1000
    for i = 1:size(robotList, 2)
@@ -51,22 +59,24 @@ for iter = 1:1000
           if i ~= j
               dif = double(robotList(i).returnPos() - robotList(j).returnPos());
               mag = norm(dif);
-              if mag > 4
-                 mag = 4; 
+              if mag > d_1
+                 sum = sum - d_def*dif/norm(dif);
+              else
+                 sum = sum - feval(f_d, mag)*dif/norm(dif);
               end
-              sum = sum - double(f_d(mag))*dif/norm(dif);
           end
        end
        
        %Calculate virt leader relations
-       for j = 1:size(virtList, 2)
-           dif = double(robotList(i).returnPos() - virtList(j).returnPos());
-           mag = norm(dif);
-          if mag > 4
-             mag = 4; 
-          end
-           sum = sum - double(f_h(mag))*dif/norm(dif);
-       end
+        for j = 1:size(virtList, 2)
+            dif = robotList(i).returnPos() - virtList(j).returnPos();
+            mag = norm(dif);
+            if mag > h_1
+                sum = sum - h_def*dif/norm(dif);
+            else
+                sum = sum - feval(f_h, mag)*dif/norm(dif);
+            end
+        end
               
        sum = sum - K_p*(robotList(i).vel);
        
@@ -80,8 +90,19 @@ for iter = 1:1000
        
        % Change robot's intial velocity for next cycle
        robotList(i).vel = robotList(i).vel + robotList(i).acc*t;
-       
-   end
+       if robotList(i).vel > vel_max
+           robotList(i).vel = vel_max;
+       end       
+    end
+   
+   % Move the virtual body on a trajectory
+    for i = 1:size(virtList, 2)
+        if iter < 0
+            pos = virtList(i).returnPos() + [0.01 0.01];
+            reward = source1.Reward(pos);
+            virtLead.addState([pos, reward]);
+        end
+    end
 end
 
 show = true;
